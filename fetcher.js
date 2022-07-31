146a37c6c80f71790e31e77026baec3b52463ec5
ej8899 <ej8899@gmail.com>
2022-07-31 11:34:15 -0600

    add basic save file function of test URL

--- fetcher.js
+++ fetcher.js
@@ -7,8 +7,11 @@
 //
 //  REQUIRES
 //
-const fs = require('fs');
 
+
+// ! we must npm init and then npm install request to use HTTP TCP library:
+const request = require('request');
+const fs = require('fs'); // node FILE SERVICES
 
 //
 // main function fetcher();  -- input via argv no need to pass into function
@@ -21,16 +24,29 @@ const fetcher = function() {
   let inputWords = process.argv.slice(2);
   console.log(inputWords);
 
-  // need valid URL for input data
-  // need filename for output data
+  // todo need valid URL for input data
+  // todo need filename for output data
 
-
+  //
   // fetch URL data
-  // what if bad return?  give user errors
-  
+  // todo what if bad return?  give user errors
+  //
+  request('http://www.google.com', (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+
+    if(response.statusCode === 200) {
+      saveFile(inputWords[1],body);
+    } else {
+      console.log("ERROR FETCHING DATA: " + response.statusCode);
+    }
+  });
+};
+
+const saveFile = function(filename, content) {
   // save to local file
   // research: https://nodejs.dev/learn/writing-files-with-nodejs
-  const content = "abcd jafjkldsj fdlksjflkdsjskl fjdlkjf lds;jfld; flkad;jfjdfljd lkdjfdlkjdlfkdjdlfjd ljlkjdfj";
   fs.writeFile('./test.txt', content, err => {
     if (err) {
       console.error(err);
@@ -52,13 +68,12 @@ const fetcher = function() {
     stats.isFile(); // true
     stats.isDirectory(); // false
     stats.isSymbolicLink(); // false
-    stats.size; // 1024000 //= 1MB
-
-    console.log("FILE SIZE: " + stats.size);
+    stats.size; // 1024000 //= 1MB 
   });
+  
+  const size = content.length;
+  console.log("SAVED TO: " + filename +" FILE SIZE: " + size);
+}
 
-};
-
-
-
+console.clear();
 fetcher();
